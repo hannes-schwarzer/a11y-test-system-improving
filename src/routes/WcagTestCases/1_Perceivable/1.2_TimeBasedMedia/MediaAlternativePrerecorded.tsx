0fcc4530cc0e9fd274ae61f1ede9a3977a82d786
Andreas Kraus <86951527+andiKandi@users.noreply.github.com>
2021-12-06 09:23:00 +0100

    content control MediaAlternativePrerecorded.tsx

--- src/routes/WcagTestCases/1_Perceivable/1.2_TimeBasedMedia/MediaAlternativePrerecorded.tsx
+++ src/routes/WcagTestCases/1_Perceivable/1.2_TimeBasedMedia/MediaAlternativePrerecorded.tsx
@@ -18,6 +18,9 @@ export const MediaAlternativePrerecorded = (props: Props) => {
           }
           successCriterionNumber={"1.2.8"}
           level={Level.AAA}
+          explanation={
+            "Accessible multimedia (visual and auditory content that is synchronized) must include captions—text versions of speech and other important audio content—allowing it to be accessible to people who can't hear all of the audio. Beyond people with disabilities, captioning helps people who only partially understand the language presented. Captions are also useful in noisy environments like airports, in quiet environments like libraries, and for multimodal learning."
+          }
         >
           <a href={"interview.mov"}>Watch the interview</a>
         </TestCase>
