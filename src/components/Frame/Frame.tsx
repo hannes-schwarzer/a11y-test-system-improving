0a238b9e1d5e65398ad05fba8362a545ed988e24
Andreas Kraus <86951527+andiKandi@users.noreply.github.com>
2021-12-07 12:23:14 +0100

    frame shows Number of test cases instead of duplicate page title

--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Header } from "../Header/Header";
 import { Navigation } from "../Navigation/Navigation";
 
@@ -9,6 +9,10 @@ type FrameProps = {
 };
 
 export const Frame: FC<FrameProps> = ({ pageTitle, children }) => {
+  const [numberOfTestCases, setNumberOfTestCases] = useState(0);
+  useEffect(() => {
+    setNumberOfTestCases(document.getElementsByClassName("testCase").length);
+  });
   return (
     <>
       <Header pageTitle={pageTitle} />
@@ -16,7 +20,9 @@ export const Frame: FC<FrameProps> = ({ pageTitle, children }) => {
         <Navigation />
         <main className={"main__content"}>
           <section>
-            <h2 className={"main__heading"}>{pageTitle}</h2>
+            <h2 className={"main__heading"}>
+              Number of Test Cases: {numberOfTestCases}
+            </h2>
             {children}
           </section>
         </main>
