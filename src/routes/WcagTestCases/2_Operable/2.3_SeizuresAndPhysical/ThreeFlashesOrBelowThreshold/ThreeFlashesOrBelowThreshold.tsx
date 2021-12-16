69f29fc7f0cd6205fbdefd2a570c75f4319a9763
Andreas Kraus <86951527+andiKandi@users.noreply.github.com>
2021-12-16 14:47:29 +0100

    changed frame size to circumvent this kind of error message

--- src/routes/WcagTestCases/2_Operable/2.3_SeizuresAndPhysical/ThreeFlashesOrBelowThreshold/ThreeFlashesOrBelowThreshold.tsx
+++ src/routes/WcagTestCases/2_Operable/2.3_SeizuresAndPhysical/ThreeFlashesOrBelowThreshold/ThreeFlashesOrBelowThreshold.tsx
@@ -26,14 +26,13 @@ export const ThreeFlashesOrBelowThreshold = (props: Props) => {
           }
         >
           <iframe
-            width="560"
-            height="315"
+            height="100%"
             src="https://www.youtube.com/embed/4LQLvgXLguk"
-            title="YouTube video player"
+            title="Banned Pokemon Seizure Scene"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-          ></iframe>
+          />
         </TestCase>
       </Frame>
     </>
