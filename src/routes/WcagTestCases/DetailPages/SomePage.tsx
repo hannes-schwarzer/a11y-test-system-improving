b33c51e310e4f002f44410b79a6753b0c21c5d0a
Andreas Kraus <86951527+andiKandi@users.noreply.github.com>
2021-12-23 21:13:52 +0100

    some-page main added

--- src/routes/WcagTestCases/DetailPages/SomePage.tsx
+++ src/routes/WcagTestCases/DetailPages/SomePage.tsx
@@ -1,11 +1,11 @@
 export const SomePage = () => {
   return (
-    <>
+    <main>
       <h1>Hello and welcome on SomePage!</h1>
       <p>
         This is a random page for tests without any more content than what you
         already read.
       </p>
-    </>
+    </main>
   );
 };
