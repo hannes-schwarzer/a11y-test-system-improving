771f5e87c9cc0909c2a03d815a80c12166479a78
Andreas Kraus <86951527+andiKandi@users.noreply.github.com>
2022-02-17 15:48:20 +0100

    deleted test tool remnants

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,9 @@ import ReactDOM from "react-dom";
 import { Router } from "./routes/Router";
 import "./index.css";
 
-/**
- * Use @axe-core/react for each navigated path
- * */
-if (process.env.NODE_ENV !== "production") {
-  //For Development
-  import("@axe-core/react").then((axe) => {
-    axe.default(React, ReactDOM, 1000);
-    ReactDOM.render(
-      <React.StrictMode>
-        <Router />
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  });
-} else {
-  //For Production
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,26 +1,8 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import { Route } from "react-router";
 import { routes } from "./routes";
-import { useEffect } from "react";
-import axe from "axe-core";
 
 export const Router = () => {
-  useEffect(() => {
-    /**
-     * implementation of axe-core API
-     */
-    axe
-      .run()
-      .then((results) => {
-        if (results.violations.length || results.incomplete.length) {
-          console.log(results);
-        }
-      })
-      .catch((err) => {
-        console.error("axe-core error:", err.message);
-      });
-  });
-
   const routeComponents = routes.map(
     ({ path, name, component, className }, key) => {
       if (
