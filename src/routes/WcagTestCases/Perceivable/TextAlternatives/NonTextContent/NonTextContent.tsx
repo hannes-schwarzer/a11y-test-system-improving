a9a845061770bef2910a68d2c447436aa16d54d8
andiKandi <akraus931@gmail.com>
2022-03-03 20:05:20 +0100

    linebreak

--- src/routes/WcagTestCases/Perceivable/TextAlternatives/NonTextContent/NonTextContent.tsx
+++ src/routes/WcagTestCases/Perceivable/TextAlternatives/NonTextContent/NonTextContent.tsx
@@ -44,7 +44,8 @@ export const NonTextContent = (props: Props) => {
 
         <TestCase
           title={
-            "F30: Failure of Success Criterion 1.1.1 and 1.2.1 due to using text alternatives that are not alternatives (e.g., filenames or placeholder text)"
+            "F30: Failure of Success Criterion 1.1.1 and 1.2.1 due to using text alternatives " +
+              "that are not alternatives (e.g., filenames or placeholder text)"
           }
           successCriterionInWords="Non-text Content"
           linkToSuccessCriterion={
