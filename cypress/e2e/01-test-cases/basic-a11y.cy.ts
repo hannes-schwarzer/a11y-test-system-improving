14f18c52a7b838b8332d124db56ca22b15437970
Hannes Schwarzer <hschwarzer@inovex.de>
2023-04-20 17:15:45 +0200

    add test case for dialog focus trap

--- cypress/e2e/01-test-cases/basic-a11y.cy.ts
+++ cypress/e2e/01-test-cases/basic-a11y.cy.ts
@@ -1,22 +1,22 @@
 /// <reference types="cypress" />
 
 describe('first', () => {
-    it.skip('should close modal with button and keyboard', () => {
+    it('should close modal with button and keyboard', () => {
         cy.visit('http://localhost:3000/wcag/operable/keyboard-accessible/keyboard')
         cy.dialogCanBeClosedByButton(cy.findByRole('link', {name: 'Open lightbox'}));        
     })
 
-    it.skip('closes modal with esc', () => {
+    it('closes modal with esc', () => {
         cy.visit('https://alphagov.github.io/accessibility-tool-audit/tests/keyboard-access-lightbox-esc-key-doesnt-close-the-lightbox.html')
         cy.dialogCanBeClosedByEsc(cy.findByRole('link', {name: 'Open lightbox'}))
     })
 
-    it.skip('closes modal with esc', () => {
+    it('closes modal with esc', () => {
         cy.visit('https://media.kulturbanause.de/2018/06/html-dialog-element.html')
         cy.dialogCanBeClosedByEsc(cy.findByRole('button', {name: 'Dialog mit show() öffnen'}));
     })
 
-    it.skip('gets all focusable elements', () => {
+    it('gets all focusable elements', () => {
         // removed hidden elements: checked
         // gets all links: checked
         cy.visit('http://localhost:3000/wcag/operable/keyboard-accessible/keyboard')
--- cypress/e2e/01-test-cases/dialog-has-focus-trap.cy.ts
+++ cypress/e2e/01-test-cases/dialog-has-focus-trap.cy.ts
@@ -15,3 +15,20 @@ it('dialog has a focus trap', () => {
 
     cy.get('@dialog').dialogHasAFocusTrap();
     })
+
+it.only('dialog has a focus trap', () => {
+    // cy.viewport(1920, 1500) // otherwise modal gets covered in parts and therefore is not visible
+    cy.visit('https://www.fonic.de/prepaid-tarife')
+
+    cy.findByRole('button', {name: 'FONIC Classic Sofort bestellen'}).click()
+
+    // cy.findByRole('dialog').should('not.exist')
+    // cy.get('.modal-content').should('not.exist')
+
+    cy.findByRole('button', {name: 'Tarifdetails anzeigen'}).click()
+
+    cy.get('.modal-content').as('dialog') //.should('be.visible')
+    cy.wait(1000)
+
+    cy.get('@dialog').dialogHasAFocusTrap();
+    })
