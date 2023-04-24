7ba4e3b44552a7712b958f030337a473182660e4
Hannes Schwarzer <hschwarzer@inovex.de>
2023-04-24 09:46:29 +0200

    Cleanup tests

--- cypress/e2e/01-test-cases/basic-a11y.cy.ts
+++ cypress/e2e/01-test-cases/basic-a11y.cy.ts
@@ -6,16 +6,6 @@ describe('first', () => {
         cy.dialogCanBeClosedByButton(cy.findByRole('link', {name: 'Open lightbox'}));        
     })
 
-    it('closes modal with esc', () => {
-        cy.visit('https://alphagov.github.io/accessibility-tool-audit/tests/keyboard-access-lightbox-esc-key-doesnt-close-the-lightbox.html')
-        cy.dialogCanBeClosedByEsc(cy.findByRole('link', {name: 'Open lightbox'}))
-    })
-
-    it('closes modal with esc', () => {
-        cy.visit('https://media.kulturbanause.de/2018/06/html-dialog-element.html')
-        cy.dialogCanBeClosedByEsc(cy.findByRole('button', {name: 'Dialog mit show() öffnen'}));
-    })
-
     it('gets all focusable elements', () => {
         // removed hidden elements: checked
         // gets all links: checked
--- cypress/e2e/01-test-cases/dialog-has-focus-trap.cy.ts
+++ cypress/e2e/01-test-cases/dialog-has-focus-trap.cy.ts
@@ -6,28 +6,22 @@ beforeEach(() => {
 
 it('dialog has a focus trap', () => {
     cy.visit('https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/examples/dialog/')
-
-    cy.get('#dialog1').as('dialog').should('not.be.visible')
     
     cy.findByRole('button', {name: 'Add Delivery Address'}).click()
 
-    cy.get('@dialog').should('be.visible')
+    cy.get('#dialog1').as('dialog').should('be.visible')
 
     cy.get('@dialog').dialogHasAFocusTrap();
     })
 
-it.only('dialog has a focus trap', () => {
-    // cy.viewport(1920, 1500) // otherwise modal gets covered in parts and therefore is not visible
+it('dialog has a focus trap', () => {
     cy.visit('https://www.fonic.de/prepaid-tarife')
 
     cy.findByRole('button', {name: 'FONIC Classic Sofort bestellen'}).click()
 
-    // cy.findByRole('dialog').should('not.exist')
-    // cy.get('.modal-content').should('not.exist')
-
     cy.findByRole('button', {name: 'Tarifdetails anzeigen'}).click()
 
-    cy.get('.modal-content').as('dialog') //.should('be.visible')
+    cy.get('.modal-content').as('dialog').should('be.visible')
     cy.wait(1000)
 
     cy.get('@dialog').dialogHasAFocusTrap();
